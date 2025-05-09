5bfc45fcb888089d77cbf2934a5cf8dc94949a36
yunsu <than4502@gmail.com>
2025-05-09 17:15:15 +0900

    chore: update prettier option

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,27 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    'single-quote': 'off',
+    'react/no-unstable-nested-components': 'off',
+    'operator-linebreak': [
+      'error',
+      'before',
+      {
+        overrides: {
+          '=': 'after',
+          '-': 'after',
+          '+': 'after',
+          '*': 'after',
+          '%': 'after',
+          '&&': 'after',
+          '||': 'after',
+        },
+      },
+    ],
+    'arrow-parens': ['warn', 'always'],
+    'no-empty-pattern': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-misused-promises': 'off',
+  },
 };
