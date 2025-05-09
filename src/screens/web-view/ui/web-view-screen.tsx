db56bbc0858a2ecda403ba054440b88c735050d4
yunsu <than4502@gmail.com>
2025-05-09 21:12:48 +0900

    feat: webview keyboard option

--- src/screens/web-view/ui/web-view-screen.tsx
+++ src/screens/web-view/ui/web-view-screen.tsx
@@ -58,6 +58,9 @@ export const WebViewScreen = () => {
         injectedJavaScript={injectedJavaScript}
         onMessage={handleWebViewMessage}
         startInLoadingState={true}
+        hideKeyboardAccessoryView
+        keyboardDisplayRequiresUserAction={false}
+        useWebView2
         renderLoading={() => (
           <ActivityIndicator
             className="absolute top-0 left-0 right-0 bottom-0 justify-center items-center"
